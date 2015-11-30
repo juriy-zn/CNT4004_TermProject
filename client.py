5c5e5ea6fbc4f48974dfe7485d980aa1f244d07e
Yuriy Zaynulin <juriy.zn@gmail.com>
2015-11-30 04:37:43 -0500

    Finish, polish

--- client.py
+++ client.py
@@ -1,19 +1,10 @@
 # ==================================================== file = client.py ========
 #   Application layer protocol for sharing stock values                        =
-#     -                                                                        =
+#     - Client side                                                            =
 # ==============================================================================
-#   Notes:                                                                     =
-#     1)                                                                       =
+#   Execute: python client.py <server IP>                                      =
 # -----------------------------------------------------------------------------=
-#   Example input:                                                             =
-# -----------------------------------------------------------------------------=
-#   Example output:                                                            =
-# -----------------------------------------------------------------------------=
-#   Bugs: None known                                                           =
-# -----------------------------------------------------------------------------=
-#   Build:                                                                     =
-# -----------------------------------------------------------------------------=
-#   Execute: python server.py                                                  =
+#   Notes: requires Python 3                                                   =
 # -----------------------------------------------------------------------------=
 #   Author:                                                                    =
 #      Yuriy Zaynulin                                                          =
@@ -21,42 +12,162 @@
 #      WWW: https://github.com/juriy-zn/                                       =
 #      Email: yuriyz@mail.usf.edu                                              =
 # -----------------------------------------------------------------------------=
-#   History: YZ (11/11/15) - Init                                              =
+#   History:                                                                   =
+#      YZ (11/11/15) - Init                                                    =
+#      YZ (11/20/15) - Sockets outline                                         =
+#      YZ (11/29/15) - Finish, polish                                          =
 # ==============================================================================
 
-
 # ---- Include files -----------------------------------------------------------
+
+import select
 import socket
-
-# ---- Defines -----------------------------------------------------------------
-
+import sys
 
 # ---- Global variables --------------------------------------------------------
 
+BUFFER_SIZE = 4096
+MAXIMUM_ATTEMPTS = 3
+PORT = 1050
+TIMEOUT = 5
 
-# ---- Function prototypes -----------------------------------------------------
+# ---- Functions ---------------------------------------------------------------
 
+# ==============================================================================
+#   Function to print the information about how to use the client program      =
+# -----------------------------------------------------------------------------=
+#   Inputs: -                                                                  =
+#   Returns: -                                                                 =
+# ==============================================================================
+def printInstruction():
+    print()
+    print("Usage:")
+    print("  COMMAND,parameter[,parameterX,parameterY,parameterZ];")
+    print()
+    print("Commands for server:")
+    print("  REG,<username>;")
+    print("  : register <username>")
+    print("  UNR,<username>;")
+    print("  : unregister <username>")
+    print("  QUO,<username>,<stock1>,<stock2>,...;")
+    print("  : request <stock1>,<stock2>,... using <username>")
+    print()
+    print("Commands for managing client:")
+    print("  I")
+    print("  : print this information again")
+    print("  R")
+    print("  : repeat previous command for server")
+    print("  Q")
+    print("  : quit the program")
+    print()
 
 # ==== Main program ============================================================
 
-HOST = '127.0.0.1'    # The remote host
-PORT = 50007          # The same port as used by the server
+try:
+#   {
+    # Instructions
+    printInstruction()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Get server IP from parameter or set to default (127.0.0.1)
+    if len(sys.argv) < 2:
+        print("WARNING: Missing an IP address, using: 127.0.0.1")
+        HOST = "127.0.0.1"
+    else:
+        print("INFO: Connecting to server: " + sys.argv[1])
+        HOST = sys.argv[1]
 
-s.connect((HOST, PORT))
+    connection = (HOST, PORT)
 
-s.sendall(b'Hello, world')
+    # Create UDP socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setblocking(0)
+    except OSError as error:
+        print("ERROR: Failed to create socket")
+        print("  Reason: " + str(error))
+        sys.exit()
 
-data = s.recv(1024)
-s.close()
+    # Clear previous message to server
+    previous_message_to_server = ""
 
-print('Received', repr(data))
+    # Utility class to break out of outside loop
+    class InnerLoopError(Exception): pass
 
+    # Start talking to server
+    while 1:
+    #   {
+        user_input = input("(Get Stock) >>> ")
+        if not user_input:
+            continue
+        elif user_input.upper() == 'Q':
+            print("Exiting...")
+            print()
+            break
+        elif user_input.upper() == 'I':
+            printInstruction()
+            continue
+        elif user_input.upper() == 'R':
+            if previous_message_to_server:
+                message_to_server = previous_message_to_server
+            else:
+                print("WARNING: No previous message to server")
+                continue
+        else:
+            message_to_server = user_input
+            previous_message_to_server = message_to_server
 
-# ==============================================================================
-#   Function to                                                                =
-# -----------------------------------------------------------------------------=
-#   Inputs:                                                                    =
-#   Returns:                                                                   =
-# ==============================================================================
+        attempts = 0
+        try:
+        #   {
+            while attempts < MAXIMUM_ATTEMPTS:
+                try:
+                    print("INFO: Sending to server [" + HOST + "]: \'" +
+                            message_to_server + "\'")
+                    s.sendto(message_to_server.encode(), connection)
+                except OSError as error:
+                    print("ERROR: Failed to send data")
+                    print("  Reason: " + str(error))
+                    raise InnerLoopError()
+
+                # Set timeout for getting the data from server
+                ready_received_data = select.select([s], [], [], TIMEOUT)
+                if ready_received_data[0]:
+                    try:
+                        received_data = s.recvfrom(BUFFER_SIZE)
+                        break
+                    except OSError as error:
+                        print("ERROR: Failed to receive data")
+                        print("  Reason: " + str(error))
+                        raise InnerLoopError()
+
+                attempts += 1
+                print("WARNING: Timeout (attempt " + str(attempts) + " of " +
+                        str(MAXIMUM_ATTEMPTS) + ") on message: \'" +
+                        message_to_server + "\'")
+        #   }
+        # Handle the error that may happen in the nested loop above
+        except InnerLoopError:
+            continue
+
+        if attempts == MAXIMUM_ATTEMPTS:
+            print("ERROR: No response from server on message: \'" +
+                    message_to_server + "\'")
+            continue
+
+        message_from_server = received_data[0]
+        address_server = received_data[1]
+        print("INFO: Received from server [" + address_server[0] + "]: \'" +
+                message_from_server.decode() + "\'")
+    #   }
+
+    s.close()
+#   }
+# Handle exit with ^C
+except KeyboardInterrupt:
+#   {
+    print()
+    print("Exiting...")
+    print()
+    s.close()
+    sys.exit()
+#   }
--- server.py
+++ server.py
@@ -1,19 +1,10 @@
 # ==================================================== file = server.py ========
 #   Application layer protocol for sharing stock values                        =
-#     -                                                                        =
+#     - Server side                                                            =
 # ==============================================================================
-#   Notes:                                                                     =
-#     1)                                                                       =
-# -----------------------------------------------------------------------------=
-#   Example input:                                                             =
-# -----------------------------------------------------------------------------=
-#   Example output:                                                            =
-# -----------------------------------------------------------------------------=
-#   Bugs: None known                                                           =
-# -----------------------------------------------------------------------------=
-#   Build:                                                                     =
-# -----------------------------------------------------------------------------=
 #   Execute: python server.py                                                  =
+# -----------------------------------------------------------------------------=
+#   Notes: requires Python 3                                                   =
 # -----------------------------------------------------------------------------=
 #   Author:                                                                    =
 #      Yuriy Zaynulin                                                          =
@@ -21,49 +12,250 @@
 #      WWW: https://github.com/juriy-zn/                                       =
 #      Email: yuriyz@mail.usf.edu                                              =
 # -----------------------------------------------------------------------------=
-#   History: YZ (11/11/15) - Init                                              =
+#   History:                                                                   =
+#      YZ (11/11/15) - Init                                                    =
+#      YZ (11/20/15) - Sockets outline                                         =
+#      YZ (11/29/15) - Finish, polish                                          =
 # ==============================================================================
 
-
 # ---- Include files -----------------------------------------------------------
+
 import socket
-
-# ---- Defines -----------------------------------------------------------------
-
+import sys
 
 # ---- Global variables --------------------------------------------------------
 
+BUFFER_SIZE = 4096
+HOST = ''
+PORT = 1050
 
-# ---- Function prototypes -----------------------------------------------------
+RESPONSE_CODES = \
+{
+    "Request OK"          : "ROK",
+    "Invalid Command"     : "INC",
+    "Invalid Parameters"  : "INP",
+    "Invalid Username"    : "INU",
+    "User Already Exists" : "UAE",
+    "User Not Registered" : "UNR"
+}
 
+COMMANDS = \
+{
+    "Register"       : "REG",
+    "Unregister"     : "UNR",
+    "Request Stocks" : "QUO"
+}
+
+# ---- Functions ---------------------------------------------------------------
+
+# ==============================================================================
+#   Function to parse the message from client,                                 =
+#               perform error checking                                         =
+# -----------------------------------------------------------------------------=
+#   Inputs: (string) message                                                   =
+#   Returns: (RESPONSE_CODES) code[,stock values]                              =
+# ==============================================================================
+def processMessage(message):
+#   {
+    # Separate the message
+    command = message[:-1].split(sep=',')[0]
+    parameters = message[:-1].split(sep=',')[1:]
+    terminator = message[-1]
+
+    print("INFO: Processing client message:")
+    print("Command:    \"" + command + "\"")
+    print("Parameters: \"" + str(parameters) + "\"")
+    print("Terminator: \"" + terminator + "\"")
+
+    # Check if terminator is there
+    if terminator != ';':
+        return RESPONSE_CODES["Invalid Command"]
+
+    # Check if command is valid
+    if command not in COMMANDS.values():
+        return RESPONSE_CODES["Invalid Command"]
+
+    # Check if parameters are valid
+    if not parameters:
+        return RESPONSE_CODES["Invalid Parameters"]
+
+    # Register command parsing
+    if command == COMMANDS['Register']:
+        if len(parameters) > 1:
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif parameters[0].strip() == '':
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif len(parameters[0]) > 32:
+            return RESPONSE_CODES["Invalid Username"]
+        elif not parameters[0].isalnum():
+            return RESPONSE_CODES["Invalid Username"]
+        else:
+            return addUsername(parameters[0])
+    # Unregister command parsing
+    elif command == COMMANDS['Unregister']:
+        if len(parameters) > 1:
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif parameters[0].strip() == '':
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif len(parameters[0]) > 32:
+            return RESPONSE_CODES["Invalid Username"]
+        elif not parameters[0].isalnum():
+            return RESPONSE_CODES["Invalid Username"]
+        else:
+            return removeUsername(parameters[0])
+    # Request Stocks command parsing
+    elif command == COMMANDS['Request Stocks']:
+        if len(parameters) < 2:
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif parameters[0].strip() == '':
+            return RESPONSE_CODES["Invalid Parameters"]
+        elif len(parameters[0]) > 32:
+            return RESPONSE_CODES["Invalid Username"]
+        elif not parameters[0].isalnum():
+            return RESPONSE_CODES["Invalid Username"]
+        else:
+            return processStocks(parameters[0], parameters[1:])
+#   }
+
+# ==============================================================================
+#   Function to add new user to registered users list                          =
+# -----------------------------------------------------------------------------=
+#   Inputs: (string) username                                                  =
+#   Returns: (RESPONSE_CODES) code "User Already Exists" or code "Request OK"  =
+# ==============================================================================
+def addUsername(username):
+    with open("usernames", "r") as file_usernames:
+        for line in file_usernames:
+            if line.lower() == (username.lower() + "\n"):
+                return RESPONSE_CODES["User Already Exists"]
+
+    with open("usernames", "a") as file_usernames:
+        file_usernames.write(username + "\n")
+
+    return RESPONSE_CODES["Request OK"]
+
+# ==============================================================================
+#   Function to remove a user from registered users list                       =
+# -----------------------------------------------------------------------------=
+#   Inputs: (string) username                                                  =
+#   Returns: (RESPONSE_CODES) code "User Not Registered" or code "Request OK"  =
+# ==============================================================================
+def removeUsername(username):
+    size_file_usernames = 0
+    new_file_usernames = []
+
+    with open("usernames", "r") as file_usernames:
+        for line in file_usernames:
+            if line.lower() != (username.lower() + "\n"):
+                new_file_usernames.append(line)
+            size_file_usernames += 1
+
+    if size_file_usernames == len(new_file_usernames):
+        return RESPONSE_CODES['User Not Registered']
+
+    with open("usernames", "w") as file_usernames:
+        for line in new_file_usernames:
+            file_usernames.write(line)
+
+    return RESPONSE_CODES["Request OK"]
+
+# ==============================================================================
+#   Function to retrieve requested stock values                                =
+# -----------------------------------------------------------------------------=
+#   Inputs: (string) username, (list) stocks                                   =
+#   Returns: (RESPONSE_CODES) code "User Not Registered" or code "Request OK", =
+#            followed by comma separated values of stocks, not found stocks'   =
+#            values are set to -1                                              =
+# ==============================================================================
+def processStocks(username, stocks):
+    size_file_usernames = 0
+    response = ""
+    with open("usernames", "r") as file_usernames:
+        for line in file_usernames:
+            if line.lower() == (username.lower() + "\n"):
+                size_file_usernames = 1
+
+    if not size_file_usernames:
+        return RESPONSE_CODES['User Not Registered']
+
+    stocks_dictionary = {}
+    with open("stocks", "r") as file_stocks:
+        for line in file_stocks:
+            stocks_dictionary[line.split()[0].lower()] = line.split()[1].lower()
+
+    for stock in stocks:
+        if stock.lower() in stocks_dictionary.keys():
+            response = response + "," + stocks_dictionary[stock.lower()]
+        else:
+            response += ",-1"
+
+    return RESPONSE_CODES["Request OK"] + response
 
 # ==== Main program ============================================================
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+try:
+#   {
+    # Create UDP socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except OSError as error:
+        print("ERROR: Failed to create socket")
+        print("  Reason: " + str(error))
+        sys.exit()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind socket
+    try:
+        s.bind((HOST, PORT))
+    except OSError as error:
+        print("ERROR: Bind failed")
+        print("  Reason: " + str(error))
+        s.close()
+        sys.exit()
 
-s.bind((HOST, PORT))
+    # Information
+    print("INFO: Server is running")
 
-s.listen(1)
+    # Start talking to clients
+    while 1:
+    #   {
+        try:
+            received_data = s.recvfrom(BUFFER_SIZE)
+        except OSError as error:
+            print("ERROR: Failed to receive data")
+            print("  Reason: " + str(error))
+            continue
 
-conn, addr = s.accept()
+        message_from_client = received_data[0]
+        address_client = received_data[1]
+        print("INFO: Received from client [" + address_client[0] + "]: \'" +
+                message_from_client.decode() + "\'")
 
-print('Connected by', addr)
+        try:
+            response = processMessage(message_from_client.decode()) + ";"
+        except Exception as error:
+            print("ERROR: Failed to process message: \'" +
+                    message_from_client.decode() + "\'")
+            print("  Reason: " + str(error))
+            continue
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
+        try:
+            print("INFO: Sending to client [" + address_client[0] + "]: \'" +
+                    response + "\'")
+            s.sendto(response.encode(), address_client)
+        except OSError as error:
+            print("ERROR: Failed to send data")
+            print("  Reason: " + str(error))
+            continue
+    #   }
 
-conn.close()
-
-
-# ==============================================================================
-#   Function to                                                                =
-# -----------------------------------------------------------------------------=
-#   Inputs:                                                                    =
-#   Returns:                                                                   =
-# ==============================================================================
+    s.close()
+#   }
+# Handle exit with ^C
+except KeyboardInterrupt:
+#   {
+    print()
+    print("Exiting...")
+    print()
+    s.close()
+    sys.exit()
+#   }
